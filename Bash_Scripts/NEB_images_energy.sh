51722cfbc557caf2c6bdd1f26cab9964042d60fa
Abhay_Rawat <arawat1909@gmail.com>
2026-04-17 15:56:15 +0530

    Change energy extraction method in NEB script

--- Bash_Scripts/NEB_images_energy.sh
+++ Bash_Scripts/NEB_images_energy.sh
@@ -1,5 +1,4 @@
 for d in 00 01 02 03 04 05 06; do
-  E=$(grep "free  energy   TOTEN" $d/OUTCAR | tail -1 | awk '{print $5}')
+  E=$(grep "energy  without entropy" $d/OUTCAR | tail -1 | awk '{print $7}')
   echo "$d $E"
-done > neb.dat
-
+done > neb_no_entropy.dat
