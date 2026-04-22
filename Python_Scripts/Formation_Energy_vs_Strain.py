3bac99825303b335b34e13b8e5b99f94b9f15e4a
Abhay_Rawat <arawat1909@gmail.com>
2026-04-22 13:32:54 +0530

    Update Formation_Energy_vs_Strain.py

--- Python_Scripts/Formation_Energy_vs_Strain.py
+++ Python_Scripts/Formation_Energy_vs_Strain.py
@@ -7,13 +7,13 @@ fe_bisb = fe[:,1]
 fe_bias = fe[:,2]
 
 plt.figure(figsize=(8,6))
-plt.plot(x, fe_bisb, marker='s', markersize=6, color='r',
-         linewidth=2.5, markeredgecolor='black', label='BiSb')
-plt.plot(x, fe_bias, marker='o', markersize=6, color='b',
-         linewidth=2.5, markeredgecolor='black', label='BiAs')
+plt.plot(x, fe_bisb, marker='s', markersize=10, color='r',
+         linewidth=4, markeredgecolor='black', label='BiSb')
+plt.plot(x, fe_bias, marker='o', markersize=10, color='b',
+         linewidth=4, markeredgecolor='black', label='BiAs')
 
-plt.xlabel('Strain (%)', fontsize=16, fontweight='bold')
-plt.ylabel('Formation Energy (eV)', fontsize=16, fontweight='bold')
+plt.xlabel('Strain (%)', fontsize=18, fontweight='bold')
+plt.ylabel('Formation Energy (eV)', fontsize=18, fontweight='bold')
 
 plt.legend(fontsize=12)
 plt.grid(True, linestyle='--', linewidth=0.6, alpha=0.8)
